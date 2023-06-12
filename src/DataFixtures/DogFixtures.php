b73839df85c6ca4c80278f856b0a5b03393bd476
Malak Bouaker <bouaker.malak@outlook.fr>
2023-06-12 12:13:10 +0200

    #7 - ajout fixtures Race

--- src/DataFixtures/DogFixtures.php
+++ src/DataFixtures/DogFixtures.php
@@ -4,22 +4,26 @@ namespace App\DataFixtures;
 
 use App\Entity\Dog;
 use App\Repository\AnnouncementRepository;
+use App\Repository\RaceRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use App\DataFixtures\RaceFixtures;
 
 class DogFixtures extends Fixture implements DependentFixtureInterface
 {
 
-    public function __construct(AnnouncementRepository $announcementRepository)
+    public function __construct(AnnouncementRepository $announcementRepository, RaceRepository $raceRepository)
     {
         $this->announcementRepository = $announcementRepository;
+        $this->raceRepository = $raceRepository;
     }
 
     public function load(ObjectManager $manager): void
     {
 
         $announcements = $this->announcementRepository->findAll();
+        $races = $this->raceRepository->findAll();
         $dogs = [
             (new Dog())
                 ->setName("Kiro")
@@ -27,35 +31,40 @@ class DogFixtures extends Fixture implements DependentFixtureInterface
                 ->setBackground("Il provient d'un élevage de la campagne")
                 ->setIsPetFriendly(1)
                 ->setDescription("Une boule d'énergie pour égailler votre vie")
-                ->setIsAdopted(0),
+                ->setIsAdopted(0)
+                ->addRace($races[51]),
             (new Dog())
                 ->setName("Doggo")
                 ->setIsLof(1)
                 ->setBackground("Renifleur de coke à Medellin")
                 ->setIsPetFriendly(0)
                 ->setDescription("Très vif mais à quelques tocs à cause de son métier")
-                ->setIsAdopted(0),
+                ->setIsAdopted(0)
+                ->addRace($races[18]),
             (new Dog())
                 ->setName("Kim")
                 ->setIsLof(1)
                 ->setBackground("A été abandonnée au bord de la route")
                 ->setIsPetFriendly(1)
                 ->setDescription("Est très câline")
-                ->setIsAdopted(0),
+                ->setIsAdopted(0)
+                ->addRace($races[10]),
             (new Dog())
                 ->setName("Brasco")
                 ->setIsLof(0)
                 ->setBackground("Il gère un cartel")
                 ->setIsPetFriendly(1)
                 ->setDescription("Pire ennemi de Doggo")
-                ->setIsAdopted(0),
+                ->setIsAdopted(0)
+                ->addRace($races[20]),
             (new Dog())
                 ->setName("Pazzi")
                 ->setIsLof(0)
                 ->setBackground("Retrouvé dans une poubelle derrière une pizzeria")
                 ->setIsPetFriendly(1)
                 ->setDescription("Il est traumatisé des pizzas, il est donc obèse, il aboie aussi avec un accent et il a une grande gesture")
-                ->setIsAdopted(1),
+                ->setIsAdopted(1)
+                ->addRace($races[1]),
         ];
         foreach ($dogs as $dog) {
             $nb = mt_rand(0, count($announcements) - 1);
@@ -70,6 +79,7 @@ class DogFixtures extends Fixture implements DependentFixtureInterface
     {
         return [
             AnnouncementFixtures::class,
+            RaceFixtures::class,
         ];
     }
 }
---
+++ src/DataFixtures/RaceFixtures.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Race;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class RaceFixtures extends Fixture 
+{
+    public function load(ObjectManager $manager): void
+    {
+        $races = [
+            "affenpinscher",
+            "africanis",
+            "american staffordshire",
+            "airedale terrier",
+            "akita américan",
+            "akita inu",
+            "barzoi",
+            "basenji",
+            "beagle",
+            "berger allemand",
+            "berger australien",
+            "berger belge tervueren",
+            "berger shetland",
+            "berger blanc suisse",
+            "bichon",
+            "bichon maltais",
+            "border collie",
+            "bouvier australien",
+            "bouvier bernois",
+            "bouvier d'Appenzeller",
+            "bouvier d'Entlebuch",
+            "bouvier suisse",
+            "boxer",
+            "braque de weimar",
+            "briard",
+            "bouledog français",
+            "bouledog anglais",
+            "caniche",
+            "caniche nain",
+            "caniche toy",
+            "carlin",
+            "chien de Rhodésie",
+            "chien d'eau portugais",
+            "chien nu méxicain",
+            "chihuahua",
+            "chow chow",
+            "cockapoo",
+            "corgi",
+            "coton de tulear",
+            "dalamtien",
+            "danois",
+            "doberman",
+            "épagneul breton",
+            "épagneul papillon",
+            "finnois de laponie",
+            "lévrier anglais",
+            "lévrier afghan",
+            "lévrier basset",
+            "lévrier écossais",
+            "lévrier whippet",
+            "golden retriever",
+            "husky",
+            "kelpie",
+            "labrador",
+            "leonberg",
+            "lhasa",
+            "malamute",
+            "malinois",
+            "mastiff",
+            "mastiff thibétain",
+            "montagne des pyrénées",
+            "pékinois",
+            "pinscher",
+            "pinscher nain",
+            "teckel",
+            "terre neuve",
+            "pitbull",
+            "poméranien",
+            "pointer anglais",
+            "rottweiler",
+            "samoyède",
+            "saint-bernard",
+            "schipperke",
+            "spitz allemand",
+            "spitz japonais",
+            "spitz nain",
+            "schnauzer",
+            "setter anglais",
+            "setter irlandais",
+            "sharpei",
+            "shiba inu",
+            "yorkshire"
+        ];
+        foreach ($races as $race){
+            $newRace = (new Race())
+            ->setName($race);
+            $manager->persist($newRace);
+
+        }
+
+        $manager->flush();
+    }
+}
