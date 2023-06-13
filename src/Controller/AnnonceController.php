c7a84ba2b1432cf1a73eeac26f5c5a7aac1b719f
hichem <hichem.meneceur@outlook.fr>
2023-06-13 14:35:04 +0200

    #9 - Premier commit, ajout d'AnnonceController et du template

---
+++ src/Controller/AnnonceController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AnnonceController extends AbstractController
+{
+    #[Route('/annonce', name: 'app_annonce')]
+    public function index(): Response
+    {
+        return $this->render('annonce/index.html.twig', [
+            'controller_name' => 'AnnonceController',
+        ]);
+    }
+}
