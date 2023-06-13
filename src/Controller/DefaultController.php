b30bc5ba81191ce063af002f69c64ec2c381bd35
Vlndo <ernaudpiels@icloud.com>
2023-06-13 15:28:08 +0200

    #15 - création page d'accueil

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\AnnouncementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(string $image, AnnouncementRepository $announcementRepository): Response
     {
+        $annonce = $announcementRepository->findOneBy([
+            'title' => $image
+        ]);
         return $this->render('default/index.html.twig', [
         ]);
     }
