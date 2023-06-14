ad6f7c6fe4e3f03162cf285f3339b3fe8d220866
Vlndo <ernaudpiels@icloud.com>
2023-06-14 16:42:54 +0200

    #15 - ajout des annonces + tableau des annonceurs dans un style t'as PEUUUUUR

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Repository\AnnouncementRepository;
 // use App\Repository\ImageRepository;
+use App\Repository\ImageRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,11 +13,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(AnnouncementRepository $announcementRepository): Response
+    public function index(AnnouncementRepository $announcementRepository, UserRepository $userRepository): Response
     {
         $annonces = $announcementRepository->findForHome();
+        $users = $userRepository->findUser();
+
         return $this->render('default/index.html.twig', [
             'annonces' => $annonces,
+            'users' => $users,
         ]);
     }
 }
