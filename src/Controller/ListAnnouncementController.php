e9addd1dae275a73720dba14b9ed02db29fba789
hichem <hichem.meneceur@outlook.fr>
2023-06-17 17:51:06 +0200

    #17 - Initialisation de la branche, ajout du controller et de l'affichage des titres des annonces

---
+++ src/Controller/ListAnnouncementController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\AnnouncementRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ListAnnouncementController extends AbstractController
+{
+    #[Route('/list', name: 'app_list_announcement')]
+    public function listAnnouncement(AnnouncementRepository $announcementRepository): Response
+    {
+
+        $annonces = $announcementRepository->findAll();
+
+        return $this->render('list_announcement/list_announcement.html.twig', [
+            'annonces' => $annonces,
+        ]);
+    }
+}
