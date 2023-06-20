7e2ebccca6ad58fc1b1fd3cfad6d45fcd1fbfcda
hichem <hichem.meneceur@outlook.fr>
2023-06-20 10:25:12 +0200

    #17 - Commentaires supprimés

--- src/Controller/ListAnnouncementController.php
+++ src/Controller/ListAnnouncementController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Repository\AnnouncementRepository;
-use App\Repository\DogRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,15 +11,13 @@ class ListAnnouncementController extends AbstractController
 {
     #[Route('/list', name: 'app_list_announcement')]
     public function listAnnouncement(AnnouncementRepository $announcementRepository): Response
-    { //, DogRepository $dogRepository
+    {
 
         $annonces = $announcementRepository->groupByAnnouncement();
-        // $group = $dogRepository->groupByDogs();
 
 
         return $this->render('list_announcement/list_announcement.html.twig', [
             'annonces' => $annonces,
-            // 'group' => $group,
         ]);
     }
 }
