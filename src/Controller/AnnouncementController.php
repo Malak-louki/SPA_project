4fdbfacbe9cb612999f96a75854f00644066c068
Malak Bouaker <bouaker.malak@outlook.fr>
2023-06-27 14:23:52 +0200

    ajout du filtre des races ne fonction de l'annonce et des filtres isAdopted et isLof

--- src/Controller/AnnouncementController.php
+++ src/Controller/AnnouncementController.php
@@ -2,23 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\Announcement;
+use App\Form\Filter\DogFilterType;
+use App\Form\Filter\AnnouncementFilter;
 use App\Repository\AnnouncementRepository;
 use App\Repository\RequestRepository;
+use App\Repository\DogRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AnnouncementController extends AbstractController
 {
-    #[Route('/annonce/{id}', name: 'app_annonce', requirements: ['id' => "\d+"])]
-    public function announcement(int $id, AnnouncementRepository $repository, RequestRepository $requestRepository): Response
+    #[Route('/annonce/{id}', name: 'app_annonce', requirements: ["id" => "\d+"])]
+    public function announcement(Announcement $annonce, AnnouncementRepository $repository, DogRepository $dogRepository, Request $request): Response
     {
-        $annonces = $repository->find($id);
-
+        
+        $filter = new AnnouncementFilter();
+        
+        $form = $this->createForm(DogFilterType::class, $filter, [
+            'announcement' => $annonce,
+        ]);
+        $form->handleRequest($request);
+        
+            $dogs = $dogRepository->filterDogs($filter, $annonce);
+        
         return $this->render('annonce/announcement.html.twig', [
-            'annonces' => $annonces,
-            'id' => $id,
-            'requests' => $requestRepository,
+            'annonces' => $annonce,
+            'form'      => $form->createView(),
+            'dogs'  => $dogs,
         ]);
     }
 }
