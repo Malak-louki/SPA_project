b6c7c552513952c9fce2054e632315130f38eeb5
Vlndo <ernaudpiels@icloud.com>
2023-06-28 14:53:42 +0200

    #59 - modification du nom des routes en fonction du nom des méthodes et des controllers + fusions des controlleurs et des vues :)

--- src/Controller/AnnouncementController.php
+++ src/Controller/AnnouncementController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Announcement;
+use App\Entity\Announcer;
 use App\Form\Filter\AnnouncementFilter;
 use App\Form\Filter\DogFilterType;
 use App\Repository\AnnouncementRepository;
@@ -12,13 +13,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\AddAnnouncementType;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use App\Form\AnnouncementFilterType;
+use Doctrine\ORM\EntityManagerInterface;
 
 class AnnouncementController extends AbstractController
 {
     #[Route('/annonce/{id}', name: 'announcement_show', requirements: ['id' => "\d+"])]
     public function show(
         int $id,
-        Announcement $announcement,
+        Announcement $announcements,
         AnnouncementRepository $announcementRepository,
         DogRepository $dogRepository,
         Request $request,
@@ -35,12 +40,69 @@ class AnnouncementController extends AbstractController
 
         $dogs = $dogRepository->filterDogs($filter, $announcement);
 
-        return $this->render('annonce/announcement.html.twig', [
+        return $this->render('announcement/announcement.html.twig', [
             'id' => $id,
             'announcement' => $announcement,
             'form' => $form->createView(),
             'dogs' => $dogs,
             'requests' => $requestRepository,
+        ]);
+    }
+
+    /** --------------------------------------- */
+
+    #[IsGranted('ROLE_ANNOUNCER')]
+    #[Route('annonceur/ajout/annonce', name: 'announcement_new')]
+    #[Route('annonceur/modification/annonce/{id}', name: 'announcement_modify')]
+    public function new(
+        Request $request,
+        AnnouncementRepository $announcementRepository,
+        ?Announcement $announcement
+    ): Response {
+        /** @var Announcer */
+        $user = $this->getUser();
+
+        if (is_null($announcement)) {
+            $announcement = new Announcement();
+        }
+
+        $announcement->setAnnouncer($user);
+        $form = $this->createForm(AddAnnouncementType::class, $announcement);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // On enregistre
+            $announcement->setUpdatedAt(new \DateTimeImmutable());
+            $announcementRepository->save($announcement, true);
+
+            return $this->redirectToRoute('announcement_listAnnouncement');
+        }
+
+        $announcement = $announcementRepository->findAll();
+
+        return $this->render('announcement/add_announcement.html.twig', [
+            'form' => $form->createView(),
+            'announcement' => $announcement,
+        ]);
+    }
+
+    /**---------------------------------*/
+
+    #[Route('/liste', name: 'announcement_listAnnouncement')]
+    public function listAnnouncement(
+        Request $request,
+        AnnouncementRepository $announcementRepository,
+        EntityManagerInterface $entityManager
+    ): Response {
+        $filter = new AnnouncementFilter();
+        $form = $this->createForm(AnnouncementFilterType::class, $filter);
+        $form->handleRequest($request);
+
+        $announcements = $announcementRepository->groupByAnnouncement($filter);
+
+        return $this->render('announcement/list_announcement.html.twig', [
+            'form' => $form->createView(),
+            'announcements' => $announcements,
         ]);
     }
 }
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -15,7 +15,7 @@ use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class RegistrationController extends AbstractController
 {
-    #[Route('/register', name: 'app_register')]
+    #[Route('/inscription', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, LoginFormAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
     {
         $user = new Adopter();
