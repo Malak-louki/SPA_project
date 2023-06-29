3a964a0a0eac3390c80191585741d31ee222634e
Vlndo <ernaudpiels@icloud.com>
2023-06-29 15:20:58 +0200

    #73 - nom de la route

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -8,8 +8,8 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'app_contact')]
-    public function index(): Response
+    #[Route('/contactez-nous', name: 'contact_contact_support')]
+    public function contactSupport(): Response
     {
         return $this->render('contact/index.html.twig', [
             'controller_name' => 'ContactController',
