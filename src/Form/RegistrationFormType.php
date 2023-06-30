5f46d9b0335e64e09b38638aac2e1b4e36d8210c
Malak Bouaker <bouaker.malak@outlook.fr>
2023-06-30 11:46:07 +0200

    TMP

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -51,8 +51,6 @@ class RegistrationFormType extends AbstractType
                     ]),
                     new Length([
                         'min' => 8,
-                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
-                        // max length allowed by Symfony for security reasons
                         'max' => 128,
                     ]),
                 ],
