9022a6f227b61e0fe44faf69b33ed663bda5bc18
Vlndo <ernaudpiels@icloud.com>
2023-07-02 15:58:53 +0200

    #73 - corrections erreurs stan apres php cs-fixer

--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\ContactRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ContactRepository::class)]
 class Contact
